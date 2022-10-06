762c7a666fdb6c5c84dab29f194c5f45031d2df7
Mayron Amati <mayronalexamati@gmail.com>
2022-10-06 10:18:00 -0300

    Tarefa: Praticando React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Um Titulo qualquer!</h1>
+      <p>Um paragrafo qualquer!</p>
+      <p>Outro paragrafo qualquer!</p>
+    </div>
+  )
+}
+
+export default App
